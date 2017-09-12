e1bdd04f254597595c7bba36a8b598a1b717e754
prathameshpm <mahamulkarprathamesh@gmail.com>
2017-09-12 14:05:42 +0530

    Outputting Data to UI

--- seed/product-seeder.js
+++ seed/product-seeder.js
@@ -11,58 +11,28 @@ var products = [
     price: 11.99
   }),
   new Product({
-    imagePath: 'http://media.moddb.com/images/articles/1/174/173130/auto/maxresdefault.jpg',
-    title: 'Age of Expires',
-    description: 'Real Time Strategy Game from Microsoft',
-    price: 11.99
+    imagePath: 'https://redmondmag.com/~/media/ECG/redmondmag/Images/introimages2014/152101REDSchwartzWin10.jpg',
+    title: 'Microsoft Windows 10',
+    description: 'An Operating System',
+    price: 199.99
   }),
   new Product({
-    imagePath: 'http://media.moddb.com/images/articles/1/174/173130/auto/maxresdefault.jpg',
-    title: 'Age of Expires',
-    description: 'Real Time Strategy Game from Microsoft',
-    price: 11.99
+    imagePath: 'http://3rmvs8um3vr2qtme426ex2k1.wpengine.netdna-cdn.com/wp-content/uploads/2016/10/From-History-of-Photoshop.png',
+    title: 'PhotoShop',
+    description: 'Image Editing Software. Sold on annual subscription liscence. Cost is per annum.',
+    price: 249
   }),
   new Product({
-    imagePath: 'http://media.moddb.com/images/articles/1/174/173130/auto/maxresdefault.jpg',
-    title: 'Age of Expires',
-    description: 'Real Time Strategy Game from Microsoft',
-    price: 11.99
+    imagePath: 'https://blogsimages.adobe.com/creativecloud/files/2016/09/devworkspace.png',
+    title: 'Adobe DreamWeaver',
+    description: 'A great IDE for web developers. Cost is per annum.',
+    price: 99
   }),
   new Product({
-    imagePath: 'http://media.moddb.com/images/articles/1/174/173130/auto/maxresdefault.jpg',
-    title: 'Age of Expires',
-    description: 'Real Time Strategy Game from Microsoft',
-    price: 11.99
-  }),
-  new Product({
-    imagePath: 'http://media.moddb.com/images/articles/1/174/173130/auto/maxresdefault.jpg',
-    title: 'Age of Expires',
-    description: 'Real Time Strategy Game from Microsoft',
-    price: 11.99
-  }),
-  new Product({
-    imagePath: 'http://media.moddb.com/images/articles/1/174/173130/auto/maxresdefault.jpg',
-    title: 'Age of Expires',
-    description: 'Real Time Strategy Game from Microsoft',
-    price: 11.99
-  }),
-  new Product({
-    imagePath: 'http://media.moddb.com/images/articles/1/174/173130/auto/maxresdefault.jpg',
-    title: 'Age of Expires',
-    description: 'Real Time Strategy Game from Microsoft',
-    price: 11.99
-  }),
-  new Product({
-    imagePath: 'http://media.moddb.com/images/articles/1/174/173130/auto/maxresdefault.jpg',
-    title: 'Age of Expires',
-    description: 'Real Time Strategy Game from Microsoft',
-    price: 11.99
-  }),
-  new Product({
-    imagePath: 'http://media.moddb.com/images/articles/1/174/173130/auto/maxresdefault.jpg',
-    title: 'Age of Expires',
-    description: 'Real Time Strategy Game from Microsoft',
-    price: 11.99
+    imagePath: 'https://www.brianchristner.io/content/images/2015/06/ubuntu-penguin.jpg',
+    title: 'Ubuntu',
+    description: 'Open-source Operating System. For all users.',
+    price: 0.00
   })
 ];
 
