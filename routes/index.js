d23f8c69598351caae52a2b9b51ee438a4e907c2
prathameshpm <mahamulkarprathamesh@gmail.com>
2017-09-14 19:43:36 +0530

    Sign In strategy in passport

--- routes/index.js
+++ routes/index.js
@@ -26,7 +26,7 @@ router.get('/', function(req, res, next) {
 
 router.get('/user/signup', function(req, res, next) {
   var messages = req.flash('error');
-  res.render('user/signup', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0})
+  res.render('user/signup', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0});
 });
 
 router.post('/user/signup', passport.authenticate('local.signup', {
@@ -38,5 +38,16 @@ router.post('/user/signup', passport.authenticate('local.signup', {
 router.get('/user/profile', function(req, res, next) {
   res.render('user/profile');
 });
+
+router.get('/user/signin', function(req, res, next) {
+  var messages = req.flash('error');
+  res.render('user/signin', {csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0});
+});
+
+router.post('/user/signin', passport.authenticate('local.signin', {
+  successRedirect: '/user/profile',
+  failureRedirect: '/user/signup',
+  failureFlash: true
+}));
 
 module.exports = router;
