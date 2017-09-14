7922fc36036af4486acc483c52b83bdb8008b6e2
prathameshpm <mahamulkarprathamesh@gmail.com>
2017-09-14 20:23:47 +0530

    Route Grouping and Protection (Middleware)

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var flash = require('connect-flash');
 var validator = require('express-validator');
 
 var index = require('./routes/index');
+var userRoutes = require('./routes/user');
 // var users = require('./routes/users');
 
 var app = express();
@@ -36,6 +37,12 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res, next) {
+  res.locals.login = req.isAuthenticated();
+  next();
+});
+
+app.use('/user', userRoutes);
 app.use('/', index);
 // app.use('/users', users);
 
