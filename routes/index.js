f118c0fdd6a56eb076aa86985f34e14ca15b8842
prathameshpm <mahamulkarprathamesh@gmail.com>
2017-09-15 15:04:37 +0530

    Build a Cart model

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var Cart = require('../models/cart');
 
 var Product = require('../models/product');
 
@@ -16,6 +17,21 @@ router.get('/', function(req, res, next) {
       title: 'Shopping Cart',
       products: productChunks
     });
+  });
+});
+
+router.get('/add-to-cart/:id', function(req, res, next) {
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : {});
+
+  Product.findById(productId, function(err, product) {
+    if (err) {
+      return res.redirect('/');
+    }
+    cart.add(product, product.id);
+    req.session.cart = cart;
+    console.log(req.session.cart);
+    res.redirect('/');
   });
 });
 
